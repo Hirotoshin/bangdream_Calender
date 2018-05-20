651d4b6dde46647712f85bfefd4f85041d82edc2
Hirotoshin <hirotoshin0211@gmail.com>
2018-05-20 17:05:20 +0900

    Initial Commit

---
+++ Bandori_cal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bandori_cal
+//
+//  Created by 伊藤潤人 on 2018/05/20.
+//  Copyright © 2018年 hirotoshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
