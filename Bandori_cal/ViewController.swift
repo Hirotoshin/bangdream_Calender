462b1ab0d76235019f9e61be03adcb58d38b1678
Hirotoshin <hirotoshin0211@gmail.com>
2018-05-20 17:08:49 +0900

    日付取得機能の実装

--- Bandori_cal/ViewController.swift
+++ Bandori_cal/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Bandori_cal
+//  Bangdream_calender
 //
 //  Created by 伊藤潤人 on 2018/05/20.
 //  Copyright © 2018年 hirotoshin. All rights reserved.
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var date: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let time = DateFormatter()
+        
+        time.timeStyle = .none
+        time.dateStyle = .full
+        time.locale = Locale(identifier:"ja_JP")
+        let now = Date()
+        
+        
+        date.text = time.string(from: now)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
